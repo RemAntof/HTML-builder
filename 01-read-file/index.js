8fff75859f0c38312d57ff39dcfc730d521940da
RemAntof <anton.ramarchuk@gmail.com>
2024-01-20 16:35:10 +0100

    fix: deleted unnesesary and add check for null

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,10 @@
 const fs = require('node:fs');
 const {join} = require('node:path');
 const correct_path = join(__dirname,'text.txt');
-const rr = fs.createReadStream(correct_path);
-rr.on('readable', () => {
-    console.log(`readable: ${rr.read()}`);
+const readable = fs.createReadStream(correct_path);
+readable.on('readable', function() {
+  let data
+  while ((data = this.read()) !== null) {
+    console.log(data.toString());
+  }
   });
