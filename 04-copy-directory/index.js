3cdab52ffbb502bedec5b73c3f2c7cc75c8ddd59
RemAntof <anton.ramarchuk@gmail.com>
2024-01-20 23:44:09 +0100

    feat: add copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,7 +3,8 @@ const { join } = require('node:path');
 const correct_path = join(__dirname, 'files_copy');
 const original_path = join(__dirname, 'files');
 console.log(correct_path);
-fs.mkdir(correct_path, (err) => {
+
+fs.mkdir(correct_path, { recursive: true }, (err) => {
   if (err) {
     return console.error(err);
   }
@@ -14,27 +15,15 @@ fs.readdir(original_path, (err, files) => {
   if (err) console.log(err);
   else {
     files.forEach((file) => {
-      let fileName;
-      let format;
-      console.log(join(original_path, file));
-      fs.stat(join(original_path, file), (err, stats) => {
-        if (err) {
-          console.error(err);
-        }
-        if (stats.isFile()) {
-          fileName = file.split('.')[0];
-          format = file.split('.')[1];
-          fs.copyFile(
-            file,
-            join(correct_path, 'copy_' + fileName + '.' + format),
-            (err) => {
-              if (err) {
-                console.log('Error Found:', err);
-              }
-            },
-          );
-        }
-      });
+      fs.copyFile(
+        join(original_path, file),
+        join(correct_path, file),
+        (err) => {
+          if (err) {
+            console.log('Error Found:', err);
+          }
+        },
+      );
     });
   }
 });
