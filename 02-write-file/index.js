f137af86468973ae7e6b69efa7c3d2d7303109a9
RemAntof <anton.ramarchuk@gmail.com>
2024-01-20 18:11:04 +0100

    feat: add writing stream

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const { join } = require('node:path');
 const correct_path = join(__dirname, 'file.txt');
-const readable = fs.createWriteStream(correct_path);
+const writableable = fs.createWriteStream(correct_path);
 
 const readline = require('readline').createInterface({
   input: process.stdin,
@@ -15,16 +15,11 @@ readline.on('line', (input) => {
   let data = input.toString();
   if (data.toLocaleUpperCase() === 'EXIT') {
     readline.close();
+  }else{
+    writableable.write("\n"+data+"\n")
   }
 });
 readline.on('close', () => {
   console.log('\nBy, hope to see you again\n');
 });
 
-// const writble = fs.createWriteStream('file.txt');
-readable.on('readable', function () {
-  let data;
-  while ((data = this.read()) !== null) {
-    console.log(data.toString());
-  }
-});
