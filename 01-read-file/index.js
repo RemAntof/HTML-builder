a8733e032fb44d9ccafd8dd99fe649f8a6f6de16
RemAntof <anton.ramarchuk@gmail.com>
2024-01-20 17:58:21 +0100

    fix: lint and prettier

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,10 @@
 const fs = require('node:fs');
-const {join} = require('node:path');
-const correct_path = join(__dirname,'text.txt');
+const { join } = require('node:path');
+const correct_path = join(__dirname, 'text.txt');
 const readable = fs.createReadStream(correct_path);
-readable.on('readable', function() {
-  let data
+readable.on('readable', function () {
+  let data;
   while ((data = this.read()) !== null) {
     console.log(data.toString());
   }
-  });
+});
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,29 +1,30 @@
 const fs = require('node:fs');
-const {join} = require('node:path');
-const correct_path = join(__dirname,'file.txt');
+const { join } = require('node:path');
+const correct_path = join(__dirname, 'file.txt');
 const readable = fs.createWriteStream(correct_path);
 
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-readline.setPrompt("\nHi there, good to see you. Now you can enter your message.\n\n")
+  input: process.stdin,
+  output: process.stdout,
+});
+readline.setPrompt(
+  '\nHi there, good to see you. Now you can enter your message.\n\n',
+);
 readline.prompt();
- readline.on("line",(input)=>{
-    let data = input.toString()
-    if(data.toLocaleUpperCase() === "EXIT"){
-        readline.close()
-    }
-
- });
- readline.on("close",()=>{
-    console.log("\nBy, hope to see you again\n")
- })
+readline.on('line', (input) => {
+  let data = input.toString();
+  if (data.toLocaleUpperCase() === 'EXIT') {
+    readline.close();
+  }
+});
+readline.on('close', () => {
+  console.log('\nBy, hope to see you again\n');
+});
 
 // const writble = fs.createWriteStream('file.txt');
-readable.on('readable', function() {
-  let data
+readable.on('readable', function () {
+  let data;
   while ((data = this.read()) !== null) {
     console.log(data.toString());
   }
-  });
+});
