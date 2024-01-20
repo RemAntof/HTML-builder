ef8a928b10c0adab5340fc57fdcdb2e7bab7930f
RemAntof <anton.ramarchuk@gmail.com>
2024-01-20 17:53:56 +0100

    feat: add welcome massege

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,26 @@
 const fs = require('node:fs');
 const {join} = require('node:path');
+const correct_path = join(__dirname,'file.txt');
+const readable = fs.createWriteStream(correct_path);
+
 const readline = require('readline').createInterface({
     input: process.stdin,
+    output: process.stdout,
   });
+readline.setPrompt("\nHi there, good to see you. Now you can enter your message.\n\n")
+readline.prompt();
  readline.on("line",(input)=>{
     let data = input.toString()
     if(data.toLocaleUpperCase() === "EXIT"){
         readline.close()
     }
+
  });
+ readline.on("close",()=>{
+    console.log("\nBy, hope to see you again\n")
+ })
 
 // const writble = fs.createWriteStream('file.txt');
-const correct_path = join(__dirname,'file.txt');
-const readable = fs.createReadStream(correct_path);
-fs.writeFile(correct_path,"",function (err) {
-    if (err) throw err;
-  });
 readable.on('readable', function() {
   let data
   while ((data = this.read()) !== null) {
