3d21f320141747206bbc8536a165b89b4849cd3c
RemAntof <anton.ramarchuk@gmail.com>
2024-01-21 17:44:33 +0100

    feat: add file read and write to bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,10 +1,34 @@
 const fs = require('node:fs');
 const { join } = require('node:path');
-const correct_path = join(__dirname, 'text.txt');
-const readable = fs.createReadStream(correct_path);
-readable.on('readable', function () {
-  let data;
-  while ((data = this.read()) !== null) {
-    console.log(data.toString());
+const correct_path = join(__dirname, 'styles');
+const write_path = join(__dirname, 'project-dist', 'bundle.css');
+const writableable = fs.createWriteStream(write_path);
+
+fs.readdir(correct_path, (err, files) => {
+  let isFormat;
+  let readable;
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      isFormat = file.split('.')[1] === 'css';
+      if (isFormat) {
+        readable = fs.createReadStream(join(correct_path, file));
+        readable.on('readable', function () {
+          let data;
+          while ((data = this.read()) !== null) {
+            // console.log(data.toString());
+            writableable.write('\n' + data + '\n');
+          }
+        });
+      }
+    });
   }
 });
+
+// const readable = fs.createReadStream(correct_path);
+// readable.on('readable', function () {
+//   let data;
+//   while ((data = this.read()) !== null) {
+//     console.log(data.toString());
+//   }
+// });
