fc7183c681ee99cf7a4c40ed02b5c6b260b0f28b
RemAntof <anton.ramarchuk@gmail.com>
2024-01-21 17:12:57 +0100

    feat: add delete not existing file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const { join } = require('node:path');
-const correct_path = join(__dirname, 'files_copy');
+const correct_path = join(__dirname, 'files-copy');
 const original_path = join(__dirname, 'files');
 console.log(correct_path);
 
@@ -14,6 +14,21 @@ fs.mkdir(correct_path, { recursive: true }, (err) => {
 fs.readdir(original_path, (err, files) => {
   if (err) console.log(err);
   else {
+    fs.readdir(correct_path,(err,copy_files) => {
+      if(err) console.log(err);
+      else{
+
+        if(files!==copy_files){
+          copy_files.forEach((copy_file)=>{
+            if(!files.includes(copy_file)){
+              fs.unlink( join(correct_path,copy_file),(err)=>{
+                if(err) console.log(err);
+              })
+            }
+          })
+        }
+      }
+    })
     files.forEach((file) => {
       fs.copyFile(
         join(original_path, file),
