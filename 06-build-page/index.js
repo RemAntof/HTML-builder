c7a8b033bc0745dc33751b5fdc959519232c9567
RemAntof <anton.ramarchuk@gmail.com>
2024-01-21 18:33:21 +0100

    init: strat task 6

---
+++ 06-build-page/index.js
@@ -0,0 +1,6 @@
+const fs = require('node:fs');
+const { join } = require('node:path');
+const correct_path = join(__dirname, 'project-dist');
+const html_path = join(correct_path,"index.html")
+const css_path = join(correct_path,"style.css")
+
