71ad76979c82de29413f109fc454d06ac45f3bab
RemAntof <anton.ramarchuk@gmail.com>
2024-01-21 17:20:05 +0100

    init: start of the task 5

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -14,21 +14,20 @@ fs.mkdir(correct_path, { recursive: true }, (err) => {
 fs.readdir(original_path, (err, files) => {
   if (err) console.log(err);
   else {
-    fs.readdir(correct_path,(err,copy_files) => {
-      if(err) console.log(err);
-      else{
-
-        if(files!==copy_files){
-          copy_files.forEach((copy_file)=>{
-            if(!files.includes(copy_file)){
-              fs.unlink( join(correct_path,copy_file),(err)=>{
-                if(err) console.log(err);
-              })
+    fs.readdir(correct_path, (err, copy_files) => {
+      if (err) console.log(err);
+      else {
+        if (files !== copy_files) {
+          copy_files.forEach((copy_file) => {
+            if (!files.includes(copy_file)) {
+              fs.unlink(join(correct_path, copy_file), (err) => {
+                if (err) console.log(err);
+              });
             }
-          })
+          });
         }
       }
-    })
+    });
     files.forEach((file) => {
       fs.copyFile(
         join(original_path, file),
