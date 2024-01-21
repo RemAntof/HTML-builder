f0fe24bf7ca91f6f894bd317fb2ed1b527346fd7
RemAntof <anton.ramarchuk@gmail.com>
2024-01-21 18:41:57 +0100

    feat: add create all falders and html and css

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,21 @@
 const fs = require('node:fs');
 const { join } = require('node:path');
-const correct_path = join(__dirname, 'project-dist');
-const html_path = join(correct_path,"index.html")
-const css_path = join(correct_path,"style.css")
+const main_folder_path = join(__dirname, 'project-dist');
+const html_path = join(main_folder_path, 'index.html');
+const css_path = join(main_folder_path, 'style.css');
+fs.mkdir(main_folder_path, { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Directory created successfully!');
+});
+const assets_path = join(main_folder_path, 'assets');
+fs.mkdir(assets_path, { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Directory created successfully!');
+});
 
+const html_write = fs.createWriteStream(html_path);
+const css_write = fs.createWriteStream(css_path);
